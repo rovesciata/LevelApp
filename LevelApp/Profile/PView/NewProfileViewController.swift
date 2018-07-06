2776776ae420f56dc1c66436f27cb666439a2a98
rovesciata <rovesciatablu@gmail.com>
2018-07-06 17:33:52 +0900

    プロフィール編集画面でキーボード被らないよう実装

--- LevelApp/Profile/PView/NewProfileViewController.swift
+++ LevelApp/Profile/PView/NewProfileViewController.swift
@@ -22,11 +22,47 @@ class NewProfileViewController: UIViewController, UITextFieldDelegate, UIImagePi
     // プロフィール写真
     @IBOutlet weak var selectImageView: UIImageView!
     
+    // 編集中のテキストフィールド
+    var editingField:UITextField?
+    // 重なっている高さ
+    var overlap: CGFloat = 0.0
+    var lastOffsetY: CGFloat = 0.0
+    
+    // キーボードの被り直し
+    // 編集開始のデリゲートメソッド
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // 編集中のテキストフィールド
+        editingField = textField
+    }
+    
+    // 編集終了のデリゲートメソッド
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        editingField = nil
+    }
+    
+    // 改行の入力のデリゲートメソッド
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true) // キーボードを下げる
+        // 改行コードは入力しない
+        return false
+        
+        // キーボードを閉じる
+                nameField.resignFirstResponder()
+                skill1Field.resignFirstResponder()
+                skill2Field.resignFirstResponder()
+                skill3Field.resignFirstResponder()
+                return true
+    }
     
     
+    // ビューのタップでキーボードを下げる
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // キーボードの被り直し
+        // スワイプでスクロールさせたならばキーボードを下げる
+        myScrollView.keyboardDismissMode = .onDrag
         // スクロールビューの領域を指定する
         let scrollFrame = CGRect(x: 0, y: 77, width: view.frame.width, height: view.frame.height-20)
         myScrollView.frame = scrollFrame
@@ -42,7 +78,72 @@ class NewProfileViewController: UIViewController, UITextFieldDelegate, UIImagePi
         skill1Field.delegate = self
         skill2Field.delegate = self
         skill3Field.delegate = self
+        
+        
+        // キーボードの被り直し
+        // デフォルトの通知センターを得る
+        let notification = NotificationCenter.default
+        // キーボードのframeが変化した
+        notification.addObserver(self,
+                                 selector: #selector(NewProfileViewController.keyboardChangeFrame(_:)),
+                                 name: NSNotification.Name.UIKeyboardDidChangeFrame, object: nil)
+        // キーボードが登場する
+        notification.addObserver(self,
+                                 selector: #selector(NewProfileViewController.keyboardWillShow(_:)),
+                                 name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        // キーボードが退場した
+        notification.addObserver(self,
+                                 selector: #selector(NewProfileViewController.keyboardDidHide(_:)),
+                                 name: NSNotification.Name.UIKeyboardDidHide, object: nil)
     }
+    
+    // キーボードの被り直し
+    // キーボードのframeが変化した通知を受けた
+    @objc func keyboardChangeFrame(_ notification: Notification) {  // 通知で実行するイベントハンドラ
+        // 編集中のテキストフィールドがない場合は中断する
+        guard let nameField = editingField else {
+            return
+        }
+        guard let skill1Field = editingField else {
+            return
+        }
+        guard let skill2Field = editingField else {
+            return
+        }
+        guard let skill3Field = editingField else {
+            return
+        }
+        // キーボードのframeを調べる
+        let userInfo = (notification as NSNotification).userInfo!
+        let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        // テキストフィールドのframeをキーボードと同じ座標軸にする
+        let nameFieldFrame = view.convert(nameField.frame, from: contentView)
+        let skill1FieldFrame = view.convert(skill1Field.frame, from: contentView)
+        let skill2FieldFrame = view.convert(skill2Field.frame, from: contentView)
+        let skill3FieldFrame = view.convert(skill3Field.frame, from: contentView)
+        // 編集中のテキストフィールドがキーボードと重なっていないか調べる
+        overlap = nameFieldFrame.maxY - keyboardFrame.minY + 5
+        if overlap > 0 {
+            // キーボードで隠れている分だけスクロールする
+            overlap += myScrollView.contentOffset.y  // すでにスクロールしている分を加算する
+            myScrollView.setContentOffset(CGPoint(x: 0, y: overlap), animated: true)
+        }
+    }
+    
+    // キーボードの被り直し
+    // キーボードが登場する通知を受けた
+    @objc func keyboardWillShow(_ notification: Notification) {  // 通知で実行するイベントハンドラ
+        // 現在のスクロール量を保存しておく
+        lastOffsetY = myScrollView.contentOffset.y
+    }
+    
+    // キーボードが退場した通知を受けた
+    @objc func keyboardDidHide(_ notification: Notification) {  // 通知で実行するイベントハンドラ
+        // スクロールを元に戻す
+        myScrollView.setContentOffset(CGPoint(x: 0, y: lastOffsetY), animated: true)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -109,16 +210,16 @@ class NewProfileViewController: UIViewController, UITextFieldDelegate, UIImagePi
     }
     
     // デリゲートメソッドを利用
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        
-        // キーボードを閉じる
-        nameField.resignFirstResponder()
-        skill1Field.resignFirstResponder()
-        skill2Field.resignFirstResponder()
-        skill3Field.resignFirstResponder()
-        return true
-    }
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//
+//
+//        // キーボードを閉じる
+//        nameField.resignFirstResponder()
+//        skill1Field.resignFirstResponder()
+//        skill2Field.resignFirstResponder()
+//        skill3Field.resignFirstResponder()
+//        return true
+//    }
     
     
     @IBAction func tapAlbumButton(_ sender: UIButton) {
