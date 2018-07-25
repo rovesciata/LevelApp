d1e03ac4ce0cbe76d78d1e0ea1b85a2acb96079d
rovesciata <rovesciatablu@gmail.com>
2018-07-26 08:57:17 +0900

    試作　カレンダーのスケジュール作成

--- LevelApp/Calendar/CalendarViewController.swift
+++ LevelApp/Calendar/CalendarViewController.swift
@@ -11,23 +11,54 @@ import FSCalendar
 import CalculateCalendarLogic
 import RealmSwift
 
+// 星ボタンのセル選択のための機能拡張
+public extension UITableView {
+    
+    func indexPathForView(_ view: UIView) -> IndexPath? {
+//        let origin = view.bounds.origin
+        // セルの位置を取得
+        let height: CGPoint = CGPoint(x: 0, y: 0)
+        let viewOrigin = self.convert(height, from: view)
+        let indexPath = self.indexPathForRow(at: viewOrigin)
+        return indexPath
+    }
+}
+
 // ディスプレイサイズ取得
 let w = UIScreen.main.bounds.size.width
 let h = UIScreen.main.bounds.size.height
 
-class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,FSCalendarDelegateAppearance{
+class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,FSCalendarDelegateAppearance {
 
+    @IBOutlet weak var scheduleTable: UITableView!
+    
+    
+    let todoCollection = TodoCollection.sharedInstance
     // スケジュール内容
     let labelDate = UILabel(frame: CGRect(x: 5, y: 580, width: 400, height: 50))
     // 「主なスケジュール」の表示
-    let labelTitle = UILabel(frame: CGRect(x: 0, y: 530, width: 180, height: 50))
+//    let labelTitle = UILabel(frame: CGRect(x: 0, y: 530, width: 180, height: 50))
     // カレンダー部分
-    let dateView = FSCalendar(frame: CGRect(x: 0, y: 30, width: w, height: 400))
+    let dateView = FSCalendar(frame: CGRect(x: 0, y: 30, width: w, height: 300))
     // 日付の表示
-    let Date = UILabel(frame: CGRect(x: 5, y: 430, width: 200, height: 100))
+//    let Date = UILabel(frame: CGRect(x: 5, y: 430, width: 200, height: 100))
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // セルの登録
+        scheduleTable.register(UINib(nibName: "TodoListTableViewCell", bundle: nil), forCellReuseIdentifier: "TodoListTableViewCell")
+        
+        todoCollection.fetchTodos()
+        
+        
+        //セルの高さを自動で計算
+        self.scheduleTable.estimatedRowHeight = 78
+        scheduleTable.rowHeight = UITableViewAutomaticDimension
+        
+//        scheduleTable.delegate = self
+//        scheduleTable.dataSource  = self
         
         //カレンダー設定
         self.dateView.dataSource = self
@@ -39,16 +70,16 @@ class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarData
         view.addSubview(dateView)
         
         // 日付表示設定
-        Date.text = ""
-        Date.font = UIFont.systemFont(ofSize: 60.0)
-        Date.textColor = .black
-        view.addSubview(Date)
+//        Date.text = ""
+//        Date.font = UIFont.systemFont(ofSize: 60.0)
+//        Date.textColor = .black
+//        view.addSubview(Date)
         
         // 「主なスケジュール」表示設定
-        labelTitle.text = ""
-        labelTitle.textAlignment = .center
-        labelTitle.font = UIFont.systemFont(ofSize: 20.0)
-        view.addSubview(labelTitle)
+//        labelTitle.text = ""
+//        labelTitle.textAlignment = .center
+//        labelTitle.font = UIFont.systemFont(ofSize: 20.0)
+//        view.addSubview(labelTitle)
         
         // スケジュール内容表示設定
         labelDate.text = ""
@@ -56,13 +87,23 @@ class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarData
         view.addSubview(labelDate)
         
         // スケジュール追加ボタン
-        let addBtn = UIButton(frame: CGRect(x: w - 70, y: h - 135, width: 60, height: 60))
+        let addBtn = UIButton(frame: CGRect(x: w - 55, y: h - 115, width: 50, height: 50))
         addBtn.setTitle("+", for: UIControlState())
         addBtn.setTitleColor(.white, for: UIControlState())
         addBtn.backgroundColor = .red
-        addBtn.layer.cornerRadius = 30.0
+        addBtn.layer.cornerRadius = 25.0
         addBtn.addTarget(self, action: #selector(onClick(_:)), for: .touchUpInside)
         view.addSubview(addBtn)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.black]
+//        self.navigationController!.navigationBar.tintColor = UIColor.black
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "新規作成", style: UIBarButtonItemStyle.plain, target: self, action: #selector(TodoListTableViewController.newTodo))
+//        self.navigationItem.leftBarButtonItem = editButtonItem
+        self.scheduleTable.reloadData()
         
     }
     
@@ -152,8 +193,8 @@ class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarData
         let da = "\(year)/\(m)/\(d)"
         
         // クリックしたら、日付が表示される
-        Date.text = "\(m)/\(d)"
-        view.addSubview(Date)
+//        Date.text = "\(m)/\(d)"
+//        view.addSubview(Date)
         
         // スケジュールを取得
         let realm = try! Realm()
@@ -168,6 +209,379 @@ class CalendarViewController: UIViewController,FSCalendarDelegate,FSCalendarData
             }
         }
     }
+    
+    
+    
+    //-------------TableViewの処理------------------
+    
+    //セクションの数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //セクションのタイトル
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?  {
+        return "25(水)"
+    }
+    
+    //セクションのタイトルの高さ
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(44)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.todoCollection.todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "myCell")
+        
+        // セルの内容表示
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListTableViewCell", for: indexPath)as! TodoListTableViewCell
+        let todo = self.todoCollection.todos[indexPath.row]
+        cell.labelCell.text = todo.title
+        cell.detailCell.text = todo.descript
+        cell.labelCell!.font = UIFont(name: "HirakakuProN-W6", size: 15)
+        
+        // 星ボタンを押した時
+        cell.starButton2.addTarget(self, action: #selector(self.buttonTapped(_:)), for: .touchUpInside)
+        
+        let defaults = UserDefaults.standard
+        
+        //        todo.finished = defaults.bool(forKey: "finishedStar")
+        let redUpAble = defaults.bool(forKey: "redUpBool")
+        cell.starButton2.isEnabled = true
+        
+        
+        if todo.finished == false {
+            // 星ボタン(くり抜き)を表示
+            if cell.labelCell.text == defaults.object(forKey: "skill1Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "赤星無し.png"), for: .normal)
+            }
+            else if cell.labelCell.text == defaults.object(forKey: "skill2Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "黄星無し.png"), for: .normal)
+            } else if cell.labelCell.text == defaults.object(forKey: "skill3Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "青星無し.png"), for: .normal)
+            }
+            
+            
+        } else {
+            // 星ボタン(塗りつぶし)を表示
+            let defaults = UserDefaults.standard
+            if cell.labelCell?.text == defaults.object(forKey: "skill1Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "赤星有り.png"), for: .normal)
+                
+            } else if cell.labelCell?.text == defaults.object(forKey: "skill2Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "黄星有り.png"), for: .normal)
+            } else if cell.labelCell?.text == defaults.object(forKey: "skill3Text") as? String {
+                cell.starButton2.setImage(UIImage(named: "青星有り.png"), for: .normal)
+            }
+            
+            //            cell.starButton2.isEnabled = false
+            
+        }
+        // 星ボタンの押したか押してないかを保存
+        defaults.set(todo.finished, forKey: "finishedStar")
+        
+        //      重要  let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "reuseIdentifier")
+        //        let todo = self.todoCollection.todos[indexPath.row]
+        //
+        //        cell.textLabel!.text = todo.title
+        //        cell.detailTextLabel!.text = todo.descript
+        //        cell.textLabel!.font = UIFont(name: "HirakakuProN-W3", size: 15)
+        
+        return cell
+    }
+    
+    // 星ボタンが押された回数の定義
+    var times = 0
+    var timesYellow = 0
+    var timesBlue = 0
+    
+    var timesSkill1 = 0
+    var timesSkill2 = 0
+    var timesSkill3 = 0
+    
+    
+    // 星ボタンを押した時の処理
+    @objc func buttonTapped(_ button: UIButton) {
+        
+        if let indexPath = self.scheduleTable.indexPathForView(button) {
+            print("Button tapped at indexPath \(indexPath.row)")
+            let todo = self.todoCollection.todos[indexPath.row]
+            todo.finished = true
+            
+            let cell = scheduleTable.dequeueReusableCell(withIdentifier: "TodoListTableViewCell", for: indexPath)as! TodoListTableViewCell
+            cell.labelCell.text = todo.title
+            
+            
+            let defaults = UserDefaults.standard
+            
+            // 星ボタンの押したか押してないかを保存
+            defaults.set(todo.finished, forKey: "finishedStar")
+            
+            
+            
+            
+            // スキルの選別をしてlevelBarを増やす
+            // skill1の場合
+            if cell.labelCell.text == defaults.object(forKey: "skill1Text") as? String {
+                
+                cell.starButton2.setImage(UIImage(named: "赤星有り.png"), for: .normal)
+                
+                
+                times = defaults.integer(forKey: "numberTimes")
+                
+                times += 1
+                
+                var count0to10: Float = 0.0
+                
+                var count10to30: Float = 0.0
+                
+                var count30to50: Float = 0.0
+                
+                var count50to70: Float = 0.0
+                
+                var count70to99: Float = 0.0
+                
+                
+                // 星ボタンを押した時、0.45を足す
+                count0to10 += 0.45
+                
+                count10to30 += 0.100
+                
+                count30to50 += 0.050
+                
+                count50to70 += 0.010
+                
+                count70to99 += 0.0036
+                
+                
+                // 星ボタンの完了カウント数をuserDefaultsで保存
+                
+                defaults.set(count0to10, forKey: "countStar")
+                defaults.set(count10to30, forKey: "countStar10to30")
+                defaults.set(count30to50, forKey: "countStar30to50")
+                defaults.set(count50to70, forKey: "countStar50to70")
+                defaults.set(count70to99, forKey: "countStar70to99")
+                
+                defaults.set(times, forKey: "numberTimes")
+                
+                
+                var countSkill10to10: Float = 0.0
+                var countSkill110to30: Float = 0.0
+                var countSkill130to50: Float = 0.0
+                var countSkill150to70: Float = 0.0
+                var countSkill170to99: Float = 0.0
+                
+                timesSkill1 = defaults.integer(forKey: "numberTimes1")
+                timesSkill1 += 1
+                
+                // 星ボタンを押した時、0.45を足す
+                countSkill10to10 += 0.45
+                
+                countSkill110to30 += 0.100
+                
+                countSkill130to50 += 0.050
+                
+                countSkill150to70 += 0.010
+                
+                countSkill170to99 += 0.0036
+                
+                // Skill1の星ボタンの完了カウント数を保存
+                defaults.set(countSkill10to10, forKey: "countStar1")
+                defaults.set(countSkill110to30, forKey: "countStar110to30")
+                defaults.set(countSkill130to50, forKey: "countStar130to50")
+                defaults.set(countSkill150to70, forKey: "countStar150to70")
+                defaults.set(countSkill170to99, forKey: "countStar170to99")
+                
+                defaults.set(timesSkill1, forKey: "numberTimes1")
+                
+                
+                // 星ボタンの押したか押してないかを保存
+                defaults.set(todo.finished, forKey: "finishedStar")
+                
+                // Skill2の星ボタンをタップした場合
+            } else if cell.labelCell.text == defaults.object(forKey: "skill2Text") as? String{
+                
+                cell.starButton2.setImage(UIImage(named: "黄星有り.png"), for: .normal)
+                
+                timesYellow = defaults.integer(forKey: "numberTimesYellow")
+                
+                timesYellow += 1
+                
+                var count0to10: Float = 0.0
+                
+                var count10to30: Float = 0.0
+                
+                var count30to50: Float = 0.0
+                
+                var count50to70: Float = 0.0
+                
+                var count70to99: Float = 0.0
+                
+                
+                // 星ボタンを押した時、0.45を足す
+                count0to10 += 0.45
+                
+                count10to30 += 0.100
+                
+                count30to50 += 0.050
+                
+                count50to70 += 0.010
+                
+                count70to99 += 0.0036
+                
+                
+                // 星ボタンの完了カウント数をuserDefaultsで保存
+                
+                defaults.set(count0to10, forKey: "countStar")
+                defaults.set(count10to30, forKey: "countStar10to30")
+                defaults.set(count30to50, forKey: "countStar30to50")
+                defaults.set(count50to70, forKey: "countStar50to70")
+                defaults.set(count70to99, forKey: "countStar70to99")
+                
+                defaults.set(timesYellow, forKey: "numberTimesYellow")
+                
+                var countSkill20to10: Float = 0.0
+                var countSkill210to30: Float = 0.0
+                var countSkill230to50: Float = 0.0
+                var countSkill250to70: Float = 0.0
+                var countSkill270to99: Float = 0.0
+                
+                timesSkill2 = defaults.integer(forKey: "numberTimes2")
+                timesSkill2 += 1
+                
+                // 星ボタンを押した時、0.45を足す
+                countSkill20to10 += 0.45
+                
+                countSkill210to30 += 0.100
+                
+                countSkill230to50 += 0.050
+                
+                countSkill250to70 += 0.010
+                
+                countSkill270to99 += 0.0036
+                
+                // Skill2の星ボタンの完了カウント数を保存
+                defaults.set(countSkill20to10, forKey: "countStar2")
+                defaults.set(countSkill210to30, forKey: "countStar210to30")
+                defaults.set(countSkill230to50, forKey: "countStar230to50")
+                defaults.set(countSkill250to70, forKey: "countStar250to70")
+                defaults.set(countSkill270to99, forKey: "countStar270to99")
+                
+                defaults.set(timesSkill2, forKey: "numberTimes2")
+                
+                
+                // 星ボタンの押したか押してないかを保存
+                defaults.set(todo.finished, forKey: "finishedStar")
+                
+                // Skill3の星ボタンを押した場合
+            } else if cell.labelCell.text == defaults.object(forKey: "skill3Text") as? String {
+                
+                cell.starButton2.setImage(UIImage(named: "青星有り.png"), for: .normal)
+                
+                
+                timesBlue = defaults.integer(forKey: "numberTimesBlue")
+                
+                timesBlue += 1
+                
+                var count0to10: Float = 0.0
+                
+                var count10to30: Float = 0.0
+                
+                var count30to50: Float = 0.0
+                
+                var count50to70: Float = 0.0
+                
+                var count70to99: Float = 0.0
+                
+                
+                // 星ボタンを押した時、0.45を足す
+                count0to10 += 0.45
+                
+                count10to30 += 0.100
+                
+                count30to50 += 0.050
+                
+                count50to70 += 0.010
+                
+                count70to99 += 0.0036
+                
+                
+                // 星ボタンの完了カウント数をuserDefaultsで保存
+                
+                defaults.set(count0to10, forKey: "countStar")
+                defaults.set(count10to30, forKey: "countStar10to30")
+                defaults.set(count30to50, forKey: "countStar30to50")
+                defaults.set(count50to70, forKey: "countStar50to70")
+                defaults.set(count70to99, forKey: "countStar70to99")
+                
+                defaults.set(timesBlue, forKey: "numberTimesBlue")
+                
+                var countSkill30to10: Float = 0.0
+                var countSkill310to30: Float = 0.0
+                var countSkill330to50: Float = 0.0
+                var countSkill350to70: Float = 0.0
+                var countSkill370to99: Float = 0.0
+                
+                timesSkill3 = defaults.integer(forKey: "numberTimes3")
+                timesSkill3 += 1
+                
+                // 星ボタンを押した時、0.45を足す
+                countSkill30to10 += 0.45
+                
+                countSkill310to30 += 0.100
+                
+                countSkill330to50 += 0.050
+                
+                countSkill350to70 += 0.010
+                
+                countSkill370to99 += 0.0036
+                
+                // Skill3の星ボタンの完了カウント数を保存
+                defaults.set(countSkill30to10, forKey: "countStar3")
+                defaults.set(countSkill310to30, forKey: "countStar310to30")
+                defaults.set(countSkill330to50, forKey: "countStar330to50")
+                defaults.set(countSkill350to70, forKey: "countStar350to70")
+                defaults.set(countSkill370to99, forKey: "countStar370to99")
+                
+                defaults.set(timesSkill3, forKey: "numberTimes3")
+                
+                // 星ボタンの押したか押してないかを保存
+                defaults.set(todo.finished, forKey: "finishedStar")
+                
+            }
+            
+            
+        }
+        
+        // 配列を全て保存
+        self.todoCollection.save()
+        
+//        self.tableView.reloadData()
+        
+    }
+    
+    
+    
+    @objc func newTodo() {
+        self.performSegue(withIdentifier: "PresentNewTodoViewController", sender: self)
+    }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        switch editingStyle {
+//        case .delete:
+//            self.todoCollection.todos.remove(at: indexPath.row)
+//            self.todoCollection.save()
+//            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.middle)
+//        default:
+//            return
+//        }
+//    }
+    
+        
+   
     
     
     // ＜最初作成したカレンダー＞
